21942999a5cfc4dac4651d7f91024a3de03dcb1e
Yaroslav Pleshakov <yvnger@gmail.com>
2024-04-21 13:09:15 +0400

    Added projects & partners layout, styles and slider

--- src/assets/scripts/slider.js
+++ src/assets/scripts/slider.js
@@ -79,3 +79,41 @@ const events = new Swiper('.events__slider', {
     }
 });
 
+const slider = new Swiper('.projects__slider', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 50,
+    navigation: {
+        nextEl: '.projects__slider-button--next',
+        prevEl: '.projects__slider-button--prev',
+    },
+    pagination: {
+        el: '.events__slider-bullets',
+        type: 'bullets',
+        bulletActiveClass: 'bullet__item--active',
+        bulletClass: 'bullet__item',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 34
+        },
+        768: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 34
+        },
+        // when window width is >= 480px
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 50
+        },
+        // when window width is >= 640px
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 50
+        }
+    }
+});
+
